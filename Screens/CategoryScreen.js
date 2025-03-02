675c0220879202b60d1774d98d5790ee1ae47edc
jainnpriyanshii <priyanshijain664@gmail.com>
2025-03-02 18:19:05 +0530

    CategoryScreen with selectable tags

--- Screens/CategoryScreen.js
+++ Screens/CategoryScreen.js
@@ -1,13 +1,71 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
 
-const CategoryScreen = () => {
+const Tags = () => {
+  const [selected, setSelected] = useState("Trending Now");
+  const tags = ["Trending Now", "All", "New", "Fashion", "Mens", "Womens","Kids","Sale","Accessories"];
+
   return (
-    <View>
-      <Text>COMING SOON</Text>
-      
+    <View style={styles.container}>
+      <FlatList
+        data={tags}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.listContainer}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={[
+              styles.tagButton,
+              item === selected ? styles.isSelected : null,
+            ]}
+            onPress={() => setSelected(item)}
+          >
+            <Text
+              style={[
+                styles.tagText,
+                item === selected ? styles.selectedText : null,
+              ]}
+            >
+              {item}
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default CategoryScreen
+export default Tags;
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 10,
+    paddingLeft: 5,
+  },
+  listContainer: {
+    flexGrow: 1,
+  },
+  tagButton: {
+    borderRadius: 14,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginVertical: 5, // Spacing between vertical items
+    backgroundColor: "#DFDCDC",
+  },
+  tagText: {
+    fontSize: 16,
+    fontWeight: "700",
+    color: "#938F8F",
+  },
+  isSelected: {
+    backgroundColor: "#E96E6E",
+  },
+  selectedText: {
+    color: "#FFFFFF",
+  },
+});
