417f06a05fa2f84ed854a521e983e194872787a3
jainnpriyanshii <priyanshijain664@gmail.com>
2025-03-02 19:09:41 +0530

    Add search functionality to HomeScreen

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text ,StyleSheet,FlatList,ScrollView,TextInput, TouchableOpacity,Image} from 'react-native'
+import { View, Text ,StyleSheet,FlatList,ScrollView,TextInput, TouchableOpacity,Image,KeyboardAvoidingView} from 'react-native'
 // import LinearGradient from 'react-native-linear-gradient';
 import React, { useState } from 'react'
 import Tags from '../Components/Tags';
@@ -9,6 +9,7 @@ import Header from '../Components/Header'
 import { useNavigation } from '@react-navigation/native';
 // import { ScrollView } from 'react-native-gesture-handler';
 
+
 export default function HomeScreen() {
   const [text ,setText] = useState('')
   const [products, setProducts] = useState(data.products);
@@ -16,6 +17,21 @@ export default function HomeScreen() {
   const handleProductDetails = (item) => {
     navigation.navigate("PRODUCT_DETAILS", { item });
   };
+  // const handleSearch = () => {
+  //   setText(query);
+  //   const formattedquery = query.toLowerCase();
+  // }
+
+  const handleSearch = (query) => {
+    setText(query);
+    const formattedQuery =query? query?.toLowerCase():'';
+    const filteredResults = data.products.filter(item => 
+      item.title && item.title.toLowerCase().includes(formattedQuery)
+    );
+    // console.log("filter",filteredResults)
+    setProducts(filteredResults);
+  };
+
   const toggleFavorite = (item) => {
     setProducts(
       products.map((prod) => {
@@ -30,7 +46,7 @@ export default function HomeScreen() {
       })
     );
   };
-  // console.log('Products Data:', data);
+  // console.log('Products Data:', products);
   // const products = data.products;
   return (
     <SafeAreaView style={styles.container}>
@@ -50,19 +66,31 @@ export default function HomeScreen() {
     />
     </TouchableOpacity> */}
    
-        <TouchableOpacity style={styles.inputcontainer}>
-          <Image 
-            source={require('../assets/search.png')} 
-            style={styles.icon} 
-          />
-         <TextInput
-          style={styles.search}
-          placeholder="Search"
-          placeholderTextColor="#888"
-          value={text}
-          onChangeText={setText}
+   <View style={styles.box}>
+    <View style={styles.inputcontainer}>
+        <TextInput 
+            style={styles.input}
+            placeholder="Search"
+            multiline={false}
+            autoFocus={true}
+            autoCapitalize="none"
+            underlineColorAndroid="transparent"
+            autoCorrect={false}
+            value = {text}
+            onChangeText={(query) => {handleSearch(query)}}
+        />
+    </View>
+    
+    <TouchableOpacity style={styles.searchButton}>
+        <Image 
+            style={styles.icon}
+            source={require('../assets/search.png')}
         />
     </TouchableOpacity>
+</View>
+
+ 
+
        
       <Tags/>
 
@@ -127,22 +155,58 @@ const styles = StyleSheet.create({
     //  marginTop:10,
       
     // },
+    
+      box: {
+          flexDirection: 'row',
+          alignItems: 'center',
+          backgroundColor: '#fff',
+          padding: 10,
+          borderRadius: 8,
+          // elevation: 3,
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.1,
+          shadowRadius: 4,
+      },
+      inputcontainer: {
+          flex: 1,
+          backgroundColor: '#f2f2f2',
+          borderRadius: 8,
+          paddingHorizontal: 10,
+          justifyContent: 'center',
+      },
+      input: {
+          height: 40,
+          fontSize: 16,
+          color: '#333',
+      },
+      searchButton: {
+          width: 40,
+          height: 40,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#E96E6E',
+          borderRadius: 8,
+          marginLeft: 10,
+      },
+      icon: {
+          width: 20,
+          height: 20,
+          tintColor: '#fff',
+      },
+  
+  
    
-    icon: {
-      width: 20,
-      height: 20,
-      marginLeft: 10,
-      marginTop:7,
-    },
-    inputcontainer:{
-      height:40,
-      width:300,
-      backgroundColor:'#EBECE9',
-      borderColor:'#E1E3DE',
-      borderWidth:2,
-      marginLeft:20,
-      borderRadius:12,
-     marginTop:10,
-     flexDirection:'row',
-    }
+   
+    // inputcontainer:{
+    //   height:40,
+    //   width:300,
+    //   backgroundColor:'#EBECE9',
+    //   borderColor:'#E1E3DE',
+    //   borderWidth:2,
+    //   marginLeft:20,
+    //   borderRadius:12,
+    //  marginTop:10,
+    //  flexDirection:'row',
+    // }
   });
